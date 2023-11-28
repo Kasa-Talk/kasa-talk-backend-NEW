a42b36346d3bc28b8a77f983aaaa918cfd8385fd
dion <juniordion7@gmail.com>
2023-11-28 17:26:26 +0800

    add fitur searching and find kata with id

--- src/routes/kata.route.js
+++ src/routes/kata.route.js
@@ -1,6 +1,14 @@
 const route = require('express').Router();
-const { getAllKata } = require('../controllers/kata.controller');
+const { 
+  getAllKata, 
+  getKataById, 
+  searchKataByIndonesia, 
+  searchKataBySasak 
+} = require('../controllers/kata.controller');
 
 route.get('/list', getAllKata);
+route.get('/:id', getKataById);
+route.get('/t/indonesia', searchKataByIndonesia);
+route.get('/t/sasak', searchKataBySasak);
 
 module.exports = route;
