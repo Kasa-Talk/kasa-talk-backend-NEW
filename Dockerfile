45ca52719f86b4ffce6bd2ca20916657a1af7656
Muh. Firdaus <muhfirdaus0805@gmail.com>
2023-12-03 19:21:46 +0800

    add Dockerfile and .dockerignore for first deployment

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["npm", "run", "prod"]
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,16 @@ const { errorrHandling } = require('../controllers/auth-Error.controller');
 
 const route = express.Router();
 
+route.use('/api', userRouter);
+route.use('/api', Kataroute);
+
 route.use('*', errorrHandling);
 route.use('*', (req, res) => {
-  res.status(403).json({
-    errors: ['Page Not Found'],
-    message: 'Forbidden',
+  res.status(200).json({
+    errors: [],
+    message: 'Success',
     data: null,
   });
 });
-route.use('/api', userRouter);
-route.use('/api', Kataroute);
 
 module.exports = route;
