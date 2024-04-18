05d853a50ce1c5feb6be009548931df7aed11d98
Muh. Firdaus <muhfirdaus0805@gmail.com>
2024-04-18 17:58:17 +0800

    update config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -45,42 +45,42 @@ module.exports = {
   //   },
   //   timezone: '+08:00',
   // },
-  test: {
-    username: process.env.DB_USERNAME_TEST,
-    password: process.env.DB_PASSWORD_TEST,
-    database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST_TEST,
-    dialect: process.env.DB_DIALECT_TEST,
-    port: process.env.DB_PORT_TEST,
-    dialectOptions: {
-      dateStrings: true,
-      typeCast(field, next) {
-        // for reading from database
-        if (field.type === 'DATETIME') {
-          return field.string();
-        }
-        return next();
-      },
-    },
-    timezone: '+08:00',
-  },
-  production: {
-    username: process.env.DB_USERNAME_PROD,
-    password: process.env.DB_PASSWORD_PROD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST_PROD,
-    dialect: process.env.DB_DIALECT_PROD,
-    port: process.env.DB_PORT_PROD,
-    dialectOptions: {
-      dateStrings: true,
-      typeCast(field, next) {
-        // for reading from database
-        if (field.type === 'DATETIME') {
-          return field.string();
-        }
-        return next();
-      },
-    },
-    timezone: '+08:00',
-  },
+//   test: {
+//     username: process.env.DB_USERNAME_TEST,
+//     password: process.env.DB_PASSWORD_TEST,
+//     database: process.env.DB_NAME_TEST,
+//     host: process.env.DB_HOST_TEST,
+//     dialect: process.env.DB_DIALECT_TEST,
+//     port: process.env.DB_PORT_TEST,
+//     dialectOptions: {
+//       dateStrings: true,
+//       typeCast(field, next) {
+//         // for reading from database
+//         if (field.type === 'DATETIME') {
+//           return field.string();
+//         }
+//         return next();
+//       },
+//     },
+//     timezone: '+08:00',
+//   },
+//   production: {
+//     username: process.env.DB_USERNAME_PROD,
+//     password: process.env.DB_PASSWORD_PROD,
+//     database: process.env.DB_NAME_PROD,
+//     host: process.env.DB_HOST_PROD,
+//     dialect: process.env.DB_DIALECT_PROD,
+//     port: process.env.DB_PORT_PROD,
+//     dialectOptions: {
+//       dateStrings: true,
+//       typeCast(field, next) {
+//         // for reading from database
+//         if (field.type === 'DATETIME') {
+//           return field.string();
+//         }
+//         return next();
+//       },
+//     },
+//     timezone: '+08:00',
+//   },
 };
